c921289c7b862bf5047a7800e4d1dd6027165cd5
Alexander Vourtsis <alexander@vourtsis.com>
2019-03-12 02:33:22 +0200

    Delete trigger.resize.js

--- trigger.resize.js
+++
@@ -1,9 +0,0 @@
-
-// on window maximize & restore, trigger resize event
-nw.Window.get().on("maximize", function() {
-	$(window).trigger('resize');
-});
-
-nw.Window.get().on("restore", function() {
-	$(window).trigger('resize');
-});
